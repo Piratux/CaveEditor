5425822ad621c9d9d4ab1b8a7ee2443de22b8e2d
Piratux <58703216+Piratux@users.noreply.github.com>
2024-05-31 18:39:04 +0300

    fix gui focus

--- Scripts/CanvasLayer.gd
+++ Scripts/CanvasLayer.gd
@@ -1,8 +1,5 @@
 extends CanvasLayer
 
-func _ready():
-	disable_element_focus(self)
-
 func capture_mouse(value):
 	# Setting process_mode to PROCESS_MODE_DISABLED is needed to prevent
 	# GUI from stealing input when mouse is hidden.
@@ -15,15 +12,17 @@ func capture_mouse(value):
 		# otherwise relative mouse movement won't work properly
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		get_tree().get_first_node_in_group("WorldManagerWindow").process_mode = Node.PROCESS_MODE_DISABLED
+		disable_element_focus(self, Control.FOCUS_NONE)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		get_tree().get_first_node_in_group("WorldManagerWindow").process_mode = Node.PROCESS_MODE_INHERIT
+		disable_element_focus(self, Control.FOCUS_ALL)
 
 # Prevent elements from being focused.
 # While focused, GUI elements may steal keyboard input, but we don't want that
-func disable_element_focus(element):
+func disable_element_focus(element, focus_type):
 	for child in element.get_children():
-		disable_element_focus(child)
+		disable_element_focus(child, focus_type)
 	
 	if element is Control:
-		element.focus_mode = Control.FOCUS_NONE
+		element.focus_mode = focus_type
